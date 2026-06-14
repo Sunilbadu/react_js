84c6225f6a27f4aaa60b623262a3bc3d9ded7d89
Sunilbadu <sunylbadu7@gmail.com>
2026-06-14 20:47:15 +0545

    update day 1

--- ss.js
+++ ss.js
@@ -2,6 +2,20 @@ function multiply(a, b) {
   return a * b;
 }
 
-const multiply =(a,b)=>{
-    a*b
-}
+const multiply =(a,b)=>  a*b;
+
+const person = {
+  name: "Ram",
+  age: 25
+};
+
+console.log(person.name);
+console.log(person.age);
+
+//using destructing
+
+const person = {
+    name: "Ram",
+    age: 25
+};
+const { name , age} = person;
