ffd01834eea6578d1f23b32c7c4f8b74a827fa1f
Sunilbadu <sunylbadu7@gmail.com>
2026-06-15 08:30:28 +0545

    update ss

--- ss.js
+++ ss.js
@@ -19,3 +19,15 @@ const person = {
     age: 25
 };
 const { name , age} = person;
+
+const fruits =["apple" , "banana" , "mango"];
+const inc = fruits.map(fr=>{
+  <li>{fr}</li>
+}
+)
+
+const nums = [2, 4, 6];
+const inc = nums.map(num=>num+1);
+
+const numsx = [1, 2, 3];
+const newx = numsx.map(n=>n*10);
